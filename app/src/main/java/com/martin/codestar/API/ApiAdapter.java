d6d9f24103cb94837885e67a3e5b0dad1b4b1c25
Martin <martinh.mireles@gmail.com>
2018-04-24 04:22:53 -0500

    Set ApiAdapter with apiservice and Add getUser to ApiInterface

--- app/src/main/java/com/martin/codestar/API/ApiAdapter.java
+++ app/src/main/java/com/martin/codestar/API/ApiAdapter.java
@@ -1,4 +1,37 @@
 package com.martin.codestar.API;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiAdapter {
+
+    private static final String BASE_URL = "https://api.github.com";
+    private static ApiInterface API_SERVICE;
+
+    /**
+     * Initialize the API_SERVICE instance to
+     * make the request to the API REST server
+     * @return ApiInterface instance
+     */
+    public static ApiInterface getApiService() {
+        if (API_SERVICE == null) {
+            OkHttpClient client;
+            HttpLoggingInterceptor interceptor;
+            Retrofit retrofit;
+
+            client = new OkHttpClient();
+            interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            client.interceptors().add(interceptor);
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+            API_SERVICE = retrofit.create(ApiInterface.class);
+        }
+        return API_SERVICE;
+    }
 }
--- app/src/main/java/com/martin/codestar/API/ApiInterface.java
+++ app/src/main/java/com/martin/codestar/API/ApiInterface.java
@@ -1,4 +1,23 @@
 package com.martin.codestar.API;
 
+import com.martin.codestar.API.models.User;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+
 public interface ApiInterface {
+
+    /**
+     * USERS endpoint to get user information
+     */
+    abstract class USERS {
+        private static final String PATH = "/users/{username}";
+
+        public static final String PARAM_USERNAME = "username";
+    }
+    @GET(USERS.PATH)
+    Call<User> getUser(@QueryMap HashMap<String, String> params);
 }
