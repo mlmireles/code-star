651107030d7d408a2e1ce48c7b30cbfa635a1166
Martin <martinh.mireles@gmail.com>
2018-04-25 19:40:18 -0500

    End MainReposModelMock

--- app/src/mock/java/com/martin/codestar/mainMock/MainReposModelMock.java
+++ app/src/mock/java/com/martin/codestar/mainMock/MainReposModelMock.java
@@ -2,6 +2,7 @@ package com.martin.codestar.mainMock;
 
 import android.os.Handler;
 
+import com.martin.codestar.API.models.RepositoriesResponse;
 import com.martin.codestar.API.models.Repository;
 import com.martin.codestar.main.IMainModel;
 import com.martin.codestar.main.IMainModelCallback;
@@ -12,20 +13,39 @@ import java.util.List;
 
 public class MainReposModelMock implements IMainModel.Repos {
 
-    private Handler mHandler;
-
     @Override
     public void getUserRepos(String username, IMainModelCallback.Repos listener) {
         final IMainModelCallback.Repos callback = listener;
 
-        this.mHandler = new Handler();
-        this.mHandler.postDelayed(new Runnable() {
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 List<Repository> repos = new ArrayList<>();
 
                 Repository a = new Repository();
-                
+                a.setName("repo");
+                a.setFull_name("a/repo");
+                a.setLanguage("Java");
+                a.setStargazers_count(10);
+                repos.add(a);
+                Repository b = new Repository();
+                b.setName("name");
+                b.setFull_name("b/name");
+                b.setLanguage("Java");
+                b.setStargazers_count(20);
+                repos.add(b);
+                Repository c = new Repository();
+                c.setName("other-name");
+                c.setFull_name("c/other-name");
+                c.setLanguage("Go");
+                c.setStargazers_count((int) Math.random());
+                repos.add(c);
+
+                RepositoriesResponse response = new RepositoriesResponse();
+                response.setRepositories(repos);
+
+                callback.onGetReposSuccess(response);
             }
         }, 2000);
     }
