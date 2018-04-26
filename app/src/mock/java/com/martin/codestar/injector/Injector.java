ef670c6d92ca5906b8b80e9c3ba80b6c49dd029a
Martin <martinh.mireles@gmail.com>
2018-04-25 19:21:09 -0500

    Add get MainReposModelMock to mock Injector

--- app/src/mock/java/com/martin/codestar/injector/Injector.java
+++ app/src/mock/java/com/martin/codestar/injector/Injector.java
@@ -1,4 +1,16 @@
 package com.martin.codestar.injector;
 
+import com.martin.codestar.main.IMainModel;
+import com.martin.codestar.mainMock.MainReposModelMock;
+
+/**
+ * Created by Martín on 25/04/2018
+ *
+ */
+
 public class Injector {
+
+    public static IMainModel.Repos provideMainReposModel() {
+        return new MainReposModelMock();
+    }
 }
--- app/src/mock/java/com/martin/codestar/mainMock/MainReposModelMock.java
+++ app/src/mock/java/com/martin/codestar/mainMock/MainReposModelMock.java
@@ -1,4 +1,30 @@
 package com.martin.codestar.mainMock;
 
-public class MainReposModelMock {
+import android.os.Handler;
+
+import com.martin.codestar.API.models.Repository;
+import com.martin.codestar.main.IMainModel;
+import com.martin.codestar.main.IMainModelCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainReposModelMock implements IMainModel.Repos {
+
+    private Handler mHandler;
+
+    @Override
+    public void getUserRepos(String username, IMainModelCallback.Repos listener) {
+        final IMainModelCallback.Repos callback = listener;
+
+        this.mHandler = new Handler();
+        this.mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                List<Repository> repos = new ArrayList<>();
+
+            }
+        }, 2000);
+    }
 }
